e99d423692e86ab7658c7e197d99b7d274b5acf0
edmund viray <eviray@my.yearupunited.org>
2026-05-01 08:28:23 -0700

    Answered bonus queries

--- viray_bonus_queries.sql
+++ viray_bonus_queries.sql
@@ -32,3 +32,41 @@ SELECT
     WHERE Prodnum LIKE '105250%'
 ;
 SELECT * FROM inventory_categories;
+
+-- 4. What is the total sales revenue across all transactions? What is the average transaction amount?
+
+SELECT
+	FORMAT(SUM(Sale_Amount), 2) AS Total_Revenue
+    , AVG(Sale_Amount) AS Mean_Transaction
+	FROM store_sales
+;
+
+-- 5. How many transactions were recorded for each product category? Which category has the most
+-- transactions?
+	-- Stationery and Supplies has the most transactions
+SELECT
+	ic.Category
+    , COUNT(*) AS Total_Transactions
+	FROM store_sales ss
+    JOIN products p
+		ON ss.Prod_Num = p.ProdNum
+    JOIN inventory_categories ic
+		ON p.categoryID = ic.CategoryID
+	GROUP BY ic.Category
+    ORDER BY Total_Transactions DESC
+;
+
+-- 6. Which store generated the highest total revenue? Which generated the lowest?
+	-- Something is wrong with the Total_Revenue column ordering
+SELECT 
+	Store_ID
+	, FORMAT(SUM(Sale_Amount), 2) AS Total_Revenue
+	FROM store_sales
+    GROUP BY Store_ID
+    ORDER BY Total_Revenue DESC
+;
+
+-- 7. What is the total revenue for each category, sorted from highest to lowest?
+
+
+-- 8. Which stores had total revenue above $50,000? (Hint: you'll need HAVING.)
